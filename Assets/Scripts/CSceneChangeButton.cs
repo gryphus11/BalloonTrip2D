042a952a9caabe8bc4b6bb0ce628f6a09f42a684
Brunhild <nechwi1121@gmail.com>
2018-09-01 09:52:33 +0900

    Scene 변경을 위한 스크립트 추가

---
+++ Assets/Scripts/CSceneChangeButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSceneChangeButton : MonoBehaviour {
+
+    public void OnSceneChecngeButtonClick(string path)
+    {
+        CSceneSwitcher.ChangeSceneByPath(path);
+    }
+}
---
+++ Assets/Scripts/CSceneSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CSceneSwitcher {
+    public static void ChangeSceneByPath(string scenePath)
+    {
+        int sceneBuildIndex = SceneUtility.GetBuildIndexByScenePath(scenePath);
+
+        if (scenePath.ToUpper().Equals("EXIT"))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        if (sceneBuildIndex == -1)
+        {
+            Debug.Log("유효하지 않은 Scene");
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
